e251fb7422847ee56aa28f08974b85acc213b265
Aleksandr Kuzmin <a-kuzmin.dev@yandex.ru>
2024-05-07 17:33:41 +0400

    Добавил компонент /AppDownload/

--- frontend/src/pages/Home/Home.jsx
+++ frontend/src/pages/Home/Home.jsx
@@ -3,6 +3,7 @@ import { useState } from 'react';
 import ExploreMenu from '../../components/ExploreMenu/ExploreMenu';
 import Header from '../../components/Header/Header';
 import FoodDisplay from '../../components/FoodDisplay/FoodDisplay';
+import AppDownload from '../../components/AppDownload/AppDownload';
 
 import './home.css';
 
@@ -15,6 +16,7 @@ const Home = () => {
             <Header />
             <ExploreMenu category={category} setCategory={setCategory} />
             <FoodDisplay category={category} />
+            <AppDownload />
         </div>
     );
 };
