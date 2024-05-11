9a8ffa93e933b94e8478b8134a561a233592681b
Aleksandr Kuzmin <a-kuzmin.dev@yandex.ru>
2024-05-11 14:12:25 +0400

    Исправил адаптацию Header и AppDownload

--- frontend/src/components/AppDownload/AppDownload.jsx
+++ frontend/src/components/AppDownload/AppDownload.jsx
@@ -6,16 +6,16 @@ const AppDownload = () => {
     const ref = useInViewBlock('mobile-app', 0.95);
     return (
         <div className="app-download" id="app-download" ref={ref}>
-            <p>
+            <h3>
                 For Better Experience Download <br /> Tomato App
-            </p>
+            </h3>
             <p className="app-download__text">
                 Install our convenient app for instant ordering of delicious food right on your device. Download the app
                 now and enjoy quick and easy orders from the comfort of your home or office.
             </p>
             <div className="app-download-platforms">
-                <button onClick={() => alert('Это демонстрационная версия, данная функция недоступна.')}><img src={assets.play_store} alt="Google Play"/></button>
-                <button onClick={() => alert('Это демонстрационная версия, данная функция недоступна.')}><img src={assets.app_store} alt="App Store" /></button>
+                <img onClick={() => alert('Это демонстрационная версия, данная функция недоступна.')} src={assets.play_store} alt="Google Play"/>
+                <img onClick={() => alert('Это демонстрационная версия, данная функция недоступна.')} src={assets.app_store} alt="App Store" />
             </div>
         </div>
     );
